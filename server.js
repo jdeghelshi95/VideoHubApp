e88fd6a825ab64538874fd863e25fc164a3a328e
jdeghelshi95 <jdeghelshi1995@gmail.com>
2022-06-06 23:34:39 -0400

    configuring server and adding mongo server

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require ('express');
 const { lstat } = require('fs');
-
+var bodyParser =  require('body-parser')
+var passport = require ('passport')
+var Video = require('./models/video.model')
 // var path = require('path')
 // var logger = require ('morgan ')
 // var cookieParser = require ('cookie-parser')
-// var passport = require ('passport')
 PORT = 5600
 
 
 var app  = express();
-var morg = require ('morgan')
 app.use(express.json())
 app.listen(PORT, () => {
     console.log('surrender to the sith on port', PORT)
