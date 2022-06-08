7044ffa5059e8fcc09110d1cc9d60ffdda03d036
jdeghelshi95 <jdeghelshi1995@gmail.com>
2022-06-08 18:21:01 -0400

    testing embedding

--- models/video.model.js
+++ models/video.model.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var VideoSchema  =new Schema({
     title: String,
     title:{
-        type: String,
+        URL: String,
         required: true,
         unique:true,
 
