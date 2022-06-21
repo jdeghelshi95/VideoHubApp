abd4be2ff44b2fbc0c3855fe6836d124fdcfc65e
jdeghelshi95 <jdeghelshi1995@gmail.com>
2022-06-21 01:57:42 -0400

    final changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,28 @@
-console.log('dfsdfss')
+
 
 document.querySelectorAll('.edit-video').forEach(btn => {
     btn.addEventListener('click', e => {
+
+
         const video = JSON.parse(e.target.dataset.video);
+  
+        // const videoForm = document.querySelector('form');
+        // videoForm.innerHTML += `
+        //     <input name="id" value=${video._id} type="hidden" />
+        // `; 
+
+        document.querySelector('input[name="id"]').value = video._id;
         document.querySelector('input[name="embedLink"]').value = video.embedLink;
         document.querySelector('textarea[name="description"]').value = video.description;
-        const videoForm = document.querySelector('form');
-        videoForm.innerHTML += `
-            <input name="id" value=${video._id} type="hidden" />
-        `; 
+
     });
 });
+
+document.getElementById('addVideo').addEventListener('click', e => {
+
+    document.querySelector('input[name="id"]').value = '';
+    document.querySelector('input[name="embedLink"]').value = '';
+    document.querySelector('textarea[name="description"]').value = '';
+
+
+})
